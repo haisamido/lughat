ea016d6cfaedf1534f1732fabecbf18801b7437e
Haisam Ido <haisam.ido@nasa.gov>
2023-04-20 18:35:18 -0400

    more changes

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,19 +1,45 @@
 -- CREATE DATABASE lughat;
 
+CREATE SCHEMA IF NOT EXISTS documents AUTHORIZATION postgres;
 CREATE SCHEMA IF NOT EXISTS writings AUTHORIZATION postgres;
 CREATE SCHEMA IF NOT EXISTS languages AUTHORIZATION postgres;
 CREATE SCHEMA IF NOT EXISTS phonology AUTHORIZATION postgres;
 CREATE SCHEMA IF NOT EXISTS morphology AUTHORIZATION postgres;
 
 CREATE SCHEMA IF NOT EXISTS typology AUTHORIZATION postgres;
-SET search_path TO public,writings,languages, phonology, morphology, typology;
+SET search_path TO public, documents, writings,languages, phonology, morphology, typology;
 
+-- TODO: philology ?
 CREATE TABLE IF NOT EXISTS writings.writing_systems (
   id SERIAL PRIMARY KEY,
   writing_system TEXT NOT NULL UNIQUE,
-  metadata JSONB NOT NULL DEFAULT '{}',
+  metadata JSONB NOT NULL DEFAULT '{}', 
   comment TEXT
 );
+
+INSERT INTO writings.writing_systems (writing_system) VALUES ('abjad');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('abugida');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('alphabet');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('logosyllabic');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('logographic');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('logoconsonantal');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('pictogramic');
+INSERT INTO writings.writing_systems (writing_system) VALUES ('syllabic');
+
+-- TODO: where to put monumental writing
+-- TODO: grapheme, glyphs (fonts), orthography
+
+-- -- heiroglyphs, hieratic, demotic, etc.
+-- CREATE TABLE IF NOT EXISTS writings.writing_types (
+--   id SERIAL PRIMARY KEY,
+--   writing_type TEXT NOT NULL UNIQUE,
+--   writing_system TEXT NOT NULL, FOREIGN KEY (writing_system) REFERENCES writing_systems(writing_system),
+--   metadata JSONB NOT NULL DEFAULT '{}',
+--   comment TEXT
+-- );
+-- INSERT INTO writings.writing_types (writing_type, writing_system) VALUES ('hieroglyphic', );
+-- INSERT INTO writings.writing_types (writing_type, writing_system) VALUES ('hieratic');
+-- INSERT INTO writings.writing_types (writing_type, writing_system) VALUES ('Demotic');
 
 CREATE TABLE IF NOT EXISTS languages.language_types (
   id SERIAL PRIMARY KEY,
@@ -21,6 +47,10 @@ CREATE TABLE IF NOT EXISTS languages.language_types (
   metadata JSONB NOT NULL DEFAULT '{}',
   comment TEXT
 );
+INSERT INTO languages.language_types (language_type) VALUES ('oral');
+INSERT INTO languages.language_types (language_type) VALUES ('written');
+INSERT INTO languages.language_types (language_type) VALUES ('sign');
+INSERT INTO languages.language_types (language_type) VALUES ('tactile');
 
 CREATE TABLE IF NOT EXISTS languages.languages (
   id SERIAL PRIMARY KEY,
